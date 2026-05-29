914053af33b305e5f8d3e9177855f5a87b02c63d
unknown <matiassimonluduena@gmail.com>
2026-05-28 21:24:57 -0300

    navbar action links

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,10 +2,12 @@ import "./navbar.css";
 import { useEffect, useState } from "react";
 
 function Navbar() {
-  const [isScrolled, setIsScrolled] = useState(false);
+  const [isScrolled, setIsScrolled] = useState(false); // Estado para detectar si el usuario hizo scroll
   const [isOpen, setIsOpen] = useState(false); // Estado para el menú móvil
+  const [activeSection, setActiveSection] = useState(""); // Estado para el link activo
 
   useEffect(() => {
+    // Logica scroll
     const handleScroll = () => {
       if (window.scrollY > 40) {
         setIsScrolled(true);
@@ -16,8 +18,32 @@ function Navbar() {
 
     window.addEventListener("scroll", handleScroll);
 
+    // logica para detectar sección activa
+    const sections = document.querySelectorAll("section[id]");
+
+    const observerOptions = {
+      root: null,
+      rootMargin: "-40% 0px -50% 0px", // Detecta la sección cuando está en el centro de la pantalla
+      threshold: 0,
+    };
+
+    const observerCallback = (entries: IntersectionObserverEntry[]): void => {
+      entries.forEach((entry: IntersectionObserverEntry) => {
+        if (entry.isIntersecting) {
+          setActiveSection(entry.target.id); // Guarda el ID activo (ej: 'projects')
+        }
+      });
+    };
+
+    const observer = new IntersectionObserver(
+      observerCallback,
+      observerOptions,
+    );
+    sections.forEach((section) => observer.observe(section));
+
     return () => {
       window.removeEventListener("scroll", handleScroll);
+      sections.forEach((section) => observer.unobserve(section));
     };
   }, []);
 
@@ -39,19 +65,39 @@ function Navbar() {
         <span></span>
       </button>
       <div className={`nav-links ${isOpen ? "active" : ""}`}>
-        <a href="#home" onClick={() => setIsOpen(false)}>
+        <a
+          href="#home"
+          onClick={() => setIsOpen(false)}
+          className={activeSection === "home" ? "active" : ""}
+        >
           Home
         </a>
-        <a href="#about" onClick={() => setIsOpen(false)}>
+        <a
+          href="#about"
+          onClick={() => setIsOpen(false)}
+          className={activeSection === "about" ? "active" : ""}
+        >
           About
         </a>
-        <a href="#skills" onClick={() => setIsOpen(false)}>
+        <a
+          href="#skills"
+          onClick={() => setIsOpen(false)}
+          className={activeSection === "skills" ? "active" : ""}
+        >
           Skills
         </a>
-        <a href="#projects" onClick={() => setIsOpen(false)}>
+        <a
+          href="#projects"
+          onClick={() => setIsOpen(false)}
+          className={activeSection === "projects" ? "active" : ""}
+        >
           Projects
         </a>
-        <a href="#contact" onClick={() => setIsOpen(false)}>
+        <a
+          href="#contact"
+          onClick={() => setIsOpen(false)}
+          className={activeSection === "contact" ? "active" : ""}
+        >
           Contact
         </a>
       </div>
